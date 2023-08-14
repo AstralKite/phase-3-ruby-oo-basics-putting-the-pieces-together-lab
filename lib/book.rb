a3357240e6a7562b7e401039b3c18fbc1fca7854
Fausto Ramirez <ramirezmedia@gmail.com>
2023-08-14 19:32:15 -0400

    commit tree 2fc7c507db48f3cbc1bbf5ca0cd4a16a4c3b9e4c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+        @author = "Agatha Christie"
+        @page_count = 272
+        @genre = "Mystery"
+    end
+
+    #Methods
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
 
+book = Book.new("this title")
+
+puts book.title
